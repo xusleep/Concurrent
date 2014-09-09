2e2b5c961e5e7796cba19f685c121be525cb7a08
Smile <Smile@Smile-VAIO>
2014-09-09 21:51:13 +0800

    Random int

---
+++ src/org/zhonglin/test/chapter15/AtomicIntRandom.java
@@ -0,0 +1,47 @@
+package zhonglin.test.chapter15;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntRandom {
+	
+	private final AtomicInteger seed = new AtomicInteger(10);
+	
+	public int nextInt(int n) {
+		while(true)
+		{
+			int i = seed.get();
+			int next = caculateNextInt(i);
+			if(seed.compareAndSet(i, next)){
+				int remainder = i % n;
+				return remainder > 0 ? remainder : remainder + n;
+			}
+		}
+	}
+	
+	private int caculateNextInt(int i){
+		Random r = new Random(i);
+		return r.nextInt();
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final AtomicIntRandom air = new AtomicIntRandom();
+		for(int i = 10; i < 20; i++)
+		{
+			final int value = i;
+			new Thread(new Runnable(){
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					System.out.println(air.nextInt(value));
+				}
+				
+			}).start();
+		}
+		
+	}
+
+}
